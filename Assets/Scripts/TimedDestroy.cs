101a0d3ce64ddf45038887d295559a9cbe294795
Grayson Sinclair <highwind360@gmail.com>
2017-05-15 22:59:04 -0700

    Made particles temporary.

---
+++ Assets/Scripts/TimedDestroy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class TimedDestroy : MonoBehaviour {
+
+	public float destroyTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, destroyTime);
+	}
+}
